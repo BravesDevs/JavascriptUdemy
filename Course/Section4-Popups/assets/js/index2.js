46c1b6debc5114520476d1b13d2ffe2beb934f36
BravesDevs <devangparekh2014@gmail.com>
2020-12-21 11:42:44 +0530

    Added add.html and index2.js file for addition of 2 numbers.

---
+++ Course/Section4-Popups/assets/js/index2.js
@@ -0,0 +1,9 @@
+var num1=parseInt(prompt("Enter number1"));
+var num2=parseInt(prompt("Enter number2"));
+
+if(num1!=null && num2!=null){
+    document.write(num1+num2);
+}
+else{
+    document.write(Math.max(num1,num2));
+}
