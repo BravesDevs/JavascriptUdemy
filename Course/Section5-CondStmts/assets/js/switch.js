9789ee18363184dfdce7b8c16cd42c01f87e7bde
BravesDevs <devangparekh2014@gmail.com>
2020-12-21 12:59:03 +0530

    Added Switch Statement.

---
+++ Course/Section5-CondStmts/assets/js/switch.js
@@ -0,0 +1,15 @@
+var x = parseInt(prompt("Enter nummber from 1 to 3"));
+switch (x) {
+    case 1:
+        document.write("One");
+        break;
+    case 2:
+        document.write("Two");
+        break;
+    case 3:
+        document.write("Three");
+        break;
+    default:
+        document.write("Invalid");
+        break;
+}
