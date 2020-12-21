de6b2f91b02921a35638c4a2436dae8fe20cf403
BravesDevs <devangparekh2014@gmail.com>
2020-12-21 12:25:55 +0530

    Added If Else Ladder Section 5

---
+++ Course/Section5-CondStmts/assets/js/index.js
@@ -0,0 +1,14 @@
+var JavaScript = parseInt(prompt("Enter JavaScript"));
+var Java = parseInt(prompt("Enter Java"));
+var sum = JavaScript + Java;
+if(sum>=80 && sum < 100)
+{
+    document.write("A");
+}
+else if(sum > 50 && sum < 80) 
+{
+    document.write("B")
+}
+else{
+    document.write("Fail")
+}
